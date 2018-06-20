9aff23455c806fe9479f7b9d0ad0427cba5e2c87
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-20 21:46:57 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,17 +1,18 @@
 # Code your solution here!
 def run_guessing_game
-  num = rand()
-  puts "Guess a number or type 'exit' to exit"
+  num = rand(0..10)
+  puts "Guess a number from 0 to 10 or type 'exit' to exit"
   input = gets.chomp
   while input
     case input
     when input.to_i 
-      
+      if input.to_i == num
+        puts
     when "exit"
       puts "Goodbye"
       break
     else
-      puts "Guess a number or type 'exit' to exit"
+      puts "Guess a number from 0 to 10 or type 'exit' to exit"
     end
   end  
 end  
